0649d8a95df129016399214818fb00670a5cadbc
shashank-crypto <55027786+shashank-crypto@users.noreply.github.com>
2021-10-09 23:51:41 +0530

    draft unit test(untested)

--- main_test.go
+++ main_test.go
@@ -18,3 +18,43 @@ func TestGetUserEndpoint(t *testing.T) {
 	assert.Equal(t, 200, response.Code, "Ok response exprected")
 }
 
+func TestGetPostEndpoint(t * testing.T){
+	router := gin.Default()
+	request, _ := http.NewRequest("GET","/posts/6161d63d42887d58b4e0c748", nil) 
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response , request)
+	assert.Equal(t, 200, response.Code, "Ok response exprected")
+}
+
+func TestpostPostEndpoint(t * testing.T){
+	router := gin.Default()
+	request, _ := http.NewRequest("POST","/posts", {
+		"caption" : "sixth Post",
+		"imageUrl" : "https://miro.medium.com/max/1132/0*Fbely1We1Tmhfnag",
+		"author" : "sha2shank2"
+	}) 
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response , request)
+	assert.Equal(t, 200, response.Code, "Ok response exprected")
+}
+
+func TestPostUserEndpoint(t * testing.T){
+	router := gin.Default()
+	request, _ := http.NewRequest("POST","/users", {
+		"id" : "newUser",
+		"name" : "shashank k",
+		"email" : "shas@gmail.com",
+		"password" : "appointy"
+	}) 
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response , request)
+	assert.Equal(t, 200, response.Code, "Ok response exprected")
+}
+
+func TestGetPostListEndpoint(t * testing.T){
+	router := gin.Default()
+	request, _ := http.NewRequest("GET","/posts/users/sha2shank2", nil) 
+	response := httptest.NewRecorder()
+	router.ServeHTTP(response , request)
+	assert.Equal(t, 200, response.Code, "Ok response exprected")
+}
